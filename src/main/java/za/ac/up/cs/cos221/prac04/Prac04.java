c3ad1bc1fa437497c7576312af6567d383f5bd3a
thuthuka <thuthuka111@gmail.com>
2022-05-02 12:33:00 +0200

    First Commit

---
+++ src/main/java/za/ac/up/cs/cos221/prac04/Prac04.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package za.ac.up.cs.cos221.prac04;
+
+/**
+ *
+ * @author Thuthuka
+ */
+public class Prac04 {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TOpFOrm().setVisible(true);
+            }
+        });
+    }
+}
